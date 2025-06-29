ad7daedfe40cb560d93d9256006f4e784892b772
AbdallahJbeli <abdallahjbeli55@gmail.com>
2025-06-29 10:10:19 +0100

    Add BestSeller, Footer, NewsLetterBox, and OurPolicy components; update Home and App for new layout

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,34 +3,35 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem';
 
-const LatestCollection = () => {
+const BestSeller = () => {
 
     const { products } = useContext(ShopContext);
-    const [latestProducts, setLatestProducts] = useState([]);
-
+    const [bestSeller, setBestSeller] = useState([]);
+    
     useEffect(() => {
-        setLatestProducts(products.slice(0, 10));
+        const bestProduct = products.filter((item) => (item.bestseller));
+        setBestSeller(bestProduct.slice(0, 5));
     }, [])
 
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+        <div className='text-center text-3xl py-8'>
+            <Title text1={'BEST'} text2={'SELLERS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.  quaerat dolorem voluptas quia nulla voluptate ex.
+                Discover our best-selling products that our customers love the most.
             </p>
         </div>
 
-        {/* Rendering Products */}
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6'>
             {
-                latestProducts.map((item, index) => (
+                bestSeller.map((item, index) => (
                     <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
                 ))
             }
         </div>
+      
     </div>
   )
 }
 
-export default LatestCollection
+export default BestSeller
