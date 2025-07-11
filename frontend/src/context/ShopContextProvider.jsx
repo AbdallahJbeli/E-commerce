c8f10444ac7905a28e1092a0c8abdd06a763a65e
AbdallahJbeli <abdallahjbeli55@gmail.com>
2025-07-11 10:59:42 +0100

    Add SearchBar component and integrate search functionality in Navbar; update ShopContext for search state management

---
+++ frontend/src/context/ShopContextProvider.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { products } from "../assets/assets";
+import { ShopContext } from "./ShopContext"; 
+const ShopContextProvider = (props) => {
+    const currency = "$";
+    const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
+
+    const value = {
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch
+    };
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+};
+
+export default ShopContextProvider;
